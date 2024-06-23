53241cf948e113c98a256b353f506d1bfcf563fe
Leticia <leticiaspeda@gmail.com>
2024-06-23 19:43:15 -0300

    Initial Commit

---
+++ TestDrive/TestDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDriveApp.swift
+//  TestDrive
+//
+//  Created by Leticia Speda on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
